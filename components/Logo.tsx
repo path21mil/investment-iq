123818822f99dafe292ae43cf60c8f2f9da6291f
Path21mil <padamkcinvesting@gmail.com>
2026-08-20 16:57:55 +0930

    logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,12 +5,12 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center hover:opacity-80 transition-opacity">
       <Image 
-        src="/logo.png" 
+        src="/inv.svg" 
         alt="Investment IQ Logo" 
-        width={240} // You can adjust this number to make it wider/smaller
-        height={60}  // You can adjust this to change the height
+        width={240} 
+        height={60}  
         className="object-contain"
-        priority // Tells Next.js to load this instantly since it's above the fold
+        priority 
       />
     </Link>
   );
