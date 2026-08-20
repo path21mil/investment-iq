a0e0d950282a88743e39698678564706cd22bebe
Path21mil <padamkcinvesting@gmail.com>
2026-08-20 17:06:48 +0930

    logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center hover:opacity-80 transition-opacity">
       <Image 
-        src="/frame.svg" 
+        src="/Frame.svg" 
         alt="Investment IQ Logo" 
         width={240} 
         height={60}  
